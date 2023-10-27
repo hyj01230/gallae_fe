6430f5c369555abd84e5a577902ab4a5634d54db
Won <hwon466@gmail.com>
2023-10-28 01:50:17 +0900

    Feat: 게시글 상세, 전체 페이지 수정

--- src/components/post/PostCategory.jsx
+++ src/components/post/PostCategory.jsx
@@ -1,3 +1,5 @@
+// PostCategory.js
+
 import { useState } from "react";
 
 export default function PostCategory({ onCategorySelect }) {
@@ -19,49 +21,58 @@ export default function PostCategory({ onCategorySelect }) {
   };
 
   return (
-    <div className="bg-white mx-10 mb-2 sticky top-0 z-10 overflow-y-auto">
+    <div className="bg-white mx-10 mb-2 sticky top-0 z-10 overflow-x-auto">
       <div className="flex justify-between">
-        <div
-          style={getCategoryStyle("전체")}
-          onClick={() => handleCategoryClick("전체")}
-        >
-          전체
-        </div>
-        <div
-          style={getCategoryStyle("가족")}
-          onClick={() => handleCategoryClick("가족")}
-        >
-          가족
-        </div>
-        <div
-          style={getCategoryStyle("친구")}
-          onClick={() => handleCategoryClick("친구")}
-        >
-          친구
-        </div>
-        <div
-          style={getCategoryStyle("연인")}
-          onClick={() => handleCategoryClick("연인")}
-        >
-          연인
-        </div>
-        <div
-          style={getCategoryStyle("혼자")}
-          onClick={() => handleCategoryClick("혼자")}
-        >
-          혼자
-        </div>
-        <div
-          style={getCategoryStyle("반려동물")}
-          onClick={() => handleCategoryClick("반려동물")}
-        >
-          반려동물
-        </div>
-        <div
-          style={getCategoryStyle("지인")}
-          onClick={() => handleCategoryClick("지인")}
-        >
-          지인
+        <div className="whitespace-nowrap overflow-x-auto">
+          <div
+            style={getCategoryStyle("전체")}
+            onClick={() => handleCategoryClick("전체")}
+            className="inline-block px-4 py-2 mr-4 cursor-pointer text-gray-600 border-b-2 border-transparent hover:border-orange-400 focus:border-orange-400"
+          >
+            전체
+          </div>
+          <div
+            style={getCategoryStyle("가족")}
+            onClick={() => handleCategoryClick("가족")}
+            className="inline-block px-4 py-2 mr-4 cursor-pointer text-gray-600 border-b-2 border-transparent hover:border-orange-400 focus:border-orange-400"
+          >
+            가족
+          </div>
+          <div
+            style={getCategoryStyle("친구")}
+            onClick={() => handleCategoryClick("친구")}
+            className="inline-block px-4 py-2 mr-4 cursor-pointer text-gray-600 border-b-2 border-transparent hover:border-orange-400 focus:border-orange-400"
+          >
+            친구
+          </div>
+          <div
+            style={getCategoryStyle("연인")}
+            onClick={() => handleCategoryClick("연인")}
+            className="inline-block px-4 py-2 mr-4 cursor-pointer text-gray-600 border-b-2 border-transparent hover:border-orange-400 focus:border-orange-400"
+          >
+            연인
+          </div>
+          <div
+            style={getCategoryStyle("혼자")}
+            onClick={() => handleCategoryClick("혼자")}
+            className="inline-block px-4 py-2 mr-4 cursor-pointer text-gray-600 border-b-2 border-transparent hover:border-orange-400 focus:border-orange-400"
+          >
+            혼자
+          </div>
+          <div
+            style={getCategoryStyle("반려동물")}
+            onClick={() => handleCategoryClick("반려동물")}
+            className="inline-block px-4 py-2 mr-4 cursor-pointer text-gray-600 border-b-2 border-transparent hover:border-orange-400 focus:border-orange-400"
+          >
+            반려동물
+          </div>
+          <div
+            style={getCategoryStyle("지인")}
+            onClick={() => handleCategoryClick("지인")}
+            className="inline-block px-4 py-2 mr-4 cursor-pointer text-gray-600 border-b-2 border-transparent hover:border-orange-400 focus:border-orange-400"
+          >
+            지인
+          </div>
         </div>
       </div>
     </div>
