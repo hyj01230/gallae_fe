20150693dd2b8a837e48f5eac2fd731bfb82cfce
hongmikyeong <mikyeong.dev@gmail.com>
2023-10-28 14:56:27 +0900

    Feat: 세부일정에서 참고링크 보이기 추가

--- src/components/schedulesDetail/List.jsx
+++ src/components/schedulesDetail/List.jsx
@@ -4,7 +4,7 @@ export default function List({ schedule, handleClick, move = true }) {
   return (
     <div
       className={`flex border border-[#F2F2F2] rounded-lg ${
-        move && "mx-4 cursor-pointer"
+        move && "mx-4 cursor-pointer  "
       } mt-4 px-4 pt-1 pb-3`}
       onClick={handleClick}
     >
@@ -18,8 +18,21 @@ export default function List({ schedule, handleClick, move = true }) {
       </div>
 
       <div className="flex flex-col w-3/4">
-        <div className="flex items-center h-8 pl-5 text-sm">
+        <div className="flex items-center h-8 pl-5 text-sm justify-between">
           {schedule.placeName}
+          {schedule.referenceURL.length > 0 && (
+            <span
+              onClick={(e) => {
+                e.stopPropagation();
+                window.open(schedule.referenceURL);
+              }}
+              // href={schedule.referenceURL}
+              // target="_blank"
+              className="text-[12px] text-[#D9D9D9] underline p-[2px]"
+            >
+              참고링크
+            </span>
+          )}
         </div>
         <div className="flex flex-col gap-1 mt-2 pl-3 text-xs text-[#666]">
           <li>{schedule.timeSpent} 소요</li>
