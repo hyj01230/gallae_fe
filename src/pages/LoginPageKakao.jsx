4d635b7ca51e6276902ca3a59ac70acf5b4e2183
pmaker126 <pmaker126@gmail.com>
2023-11-01 15:08:59 +0900

    Feat: 소셜 수정중

--- src/pages/LoginPageKakao.jsx
+++ src/pages/LoginPageKakao.jsx
@@ -18,16 +18,16 @@ export default function LoginPageKakao() {
         const response = await axiosInstance.get(
           `/kakao/callback?code=${code}`,
           {
-            grant_type: "authorization_code",
-            client_id: import.meta.env.VITE_REACT_APP_REST_API_KEY,
-            redirect_uri: import.meta.env.VITE_REACT_APP_KAKAO_REDIRECT_URI,
-            code,
-          },
-          {
             headers: {
-              "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+              "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
             },
           }
+          // {
+          //   grant_type: "authorization_code",
+          //   client_id: import.meta.env.VITE_REACT_APP_REST_API_KEY,
+          //   redirect_uri: import.meta.env.VITE_REACT_APP_KAKAO_REDIRECT_URI,
+          //   code,
+          // }
         );
         console.log("response", response);
         const accessToken = response.headers.authorization;
