4b526ac5362bcb730f5738baec2270259d205ed6
pmaker126 <pmaker126@gmail.com>
2023-11-02 17:11:07 +0900

    Feat : 카카오톡 공유하기 구현중

--- src/util/shareKakaoLink.js
+++ src/util/shareKakaoLink.js
@@ -1,4 +1,4 @@
-export const shareKakao = (title, postId) => {
+export const shareKakao = (title) => {
   // url이 id값에 따라 변경되기 때문에 route를 인자값으로 받아줌
   if (window.Kakao) {
     const kakao = window.Kakao;
@@ -6,29 +6,25 @@ export const shareKakao = (title, postId) => {
       kakao.init(import.meta.env.VITE_REACT_APP_JavaScript_KEY); // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
     }
 
-    // kakao.Share.sendCustom({
-    //   templateId: 100240,
-    //   templateArgs: {
-    //     title: "[갈래] 나랑 여행 갈래?",
-    //     description: title,
-    //   },
-    //   //   link: {
-    //   //     mobileWebUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
-    //   //     webUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
-    //   //   },
-    // });
-
-    kakao.Share.sendDefault({
-      objectType: "feed",
-      content: {
+    kakao.Share.sendCustom({
+      templateId: 100240,
+      templateArgs: {
         title: "[갈래] 나랑 여행 갈래?",
         description: title,
-        imageUrl: "public/img/gallae.png",
-        link: {
-          mobileWebUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
-          webUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
-        },
       },
     });
+
+    // kakao.Share.sendDefault({
+    //   objectType: "feed",
+    //   content: {
+    //     title: "[갈래] 나랑 여행 갈래?",
+    //     description: title,
+    //     imageUrl: "public/img/gallae.png",
+    //     link: {
+    //       mobileWebUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
+    //       webUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
+    //     },
+    //   },
+    // });
   }
 };
