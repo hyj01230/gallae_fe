3f3b9a9d52d64cb985ffb96340790493dd91801a
pmaker126 <pmaker126@gmail.com>
2023-11-02 19:20:46 +0900

    Feat : 카카오톡 공유하기 구현중

--- src/util/shareKakaoLink.js
+++ src/util/shareKakaoLink.js
@@ -1,40 +1,34 @@
 export const shareKakao = (title, postId) => {
   // url이 id값에 따라 변경되기 때문에 route를 인자값으로 받아줌
-  if (window.Kakao) {
-    const kakao = window.Kakao;
+  if (window.kakao) {
+    const kakao = window.kakao;
     if (!kakao.isInitialized()) {
       kakao.init(import.meta.env.VITE_REACT_APP_JavaScript_KEY); // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
     }
 
-    kakao.Share.sendDefault({
-      objectType: "feed",
-      content: {
-        title: "[갈래] 우리 여행 갈래?",
-        description: title,
-        imageUrl:
-          "https://github.com/hyj01230/gallae_fe/blob/main/public/img/gallae.png?raw=true",
-        link: {
-          mobileWebUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
-          webUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
-        },
+    kakao.Share.createCustomButton({
+      container: "#kakaotalk-sharing-btn",
+      templateId: 100240,
+      templateArgs: {
+        title: `${title}`,
+        postId: `${postId}`,
       },
-      social: {},
-      buttons: [
-        {
-          title: "",
-          link: {
-            mobileWebUrl: "",
-            webUrl: "",
-          },
-        },
-        {
-          title: "",
-          link: {
-            mobileWebUrl: "",
-            webUrl: "",
-          },
-        },
-      ],
     });
+
+    // kakao.Share.sendDefault({
+    //   objectType: "feed",
+    //   content: {
+    //     title: "[갈래] 우리 여행 갈래?",
+    //     description: title,
+    //     imageUrl:
+    //       "https://github.com/hyj01230/gallae_fe/blob/main/public/img/gallae.png?raw=true",
+    //     link: {
+    //       mobileWebUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
+    //       webUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
+    //     },
+    //   },
+    //   social: {},
+    //   buttons: [],
+    // });
   }
 };
