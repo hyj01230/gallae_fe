1ab9ecd3b67549e5dedbc5a23b6451435112e732
pmaker126 <pmaker126@gmail.com>
2023-11-02 19:23:51 +0900

    Feat : 카카오톡 공유하기 구현중

--- src/util/shareKakaoLink.js
+++ src/util/shareKakaoLink.js
@@ -1,13 +1,12 @@
 export const shareKakao = (title, postId) => {
   // url이 id값에 따라 변경되기 때문에 route를 인자값으로 받아줌
-  if (window.kakao) {
-    const kakao = window.kakao;
+  if (window.Kakao) {
+    const kakao = window.Kakao;
     if (!kakao.isInitialized()) {
       kakao.init(import.meta.env.VITE_REACT_APP_JavaScript_KEY); // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
     }
 
     kakao.Share.createCustomButton({
-      container: "#kakaotalk-sharing-btn",
       templateId: 100240,
       templateArgs: {
         title: `${title}`,
