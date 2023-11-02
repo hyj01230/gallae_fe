e429a2703d91a4fc9585951011a43beb3f40cd78
pmaker126 <pmaker126@gmail.com>
2023-11-02 17:28:11 +0900

    Feat: 카카오 공유하기 구현중

--- src/util/shareKakaoLink.js
+++ src/util/shareKakaoLink.js
@@ -6,14 +6,6 @@ export const shareKakao = (title, postId) => {
       kakao.init(import.meta.env.VITE_REACT_APP_JavaScript_KEY); // 카카오에서 제공받은 javascript key를 넣어줌 -> .env파일에서 호출시킴
     }
 
-    // kakao.Share.sendCustom({
-    //   templateId: 100240,
-    //   templateArgs: {
-    //     title: "[갈래] 나랑 여행 갈래?",
-    //     description: title,
-    //   },
-    // });
-
     kakao.Share.sendDefault({
       objectType: "feed",
       content: {
@@ -22,13 +14,27 @@ export const shareKakao = (title, postId) => {
         imageUrl:
           "https://github.com/hyj01230/gallae_fe/blob/main/public/img/gallae.png?raw=true",
         link: {
-          // [내 애플리케이션] > [플랫폼] 에서 등록한 사이트 도메인과 일치해야 함
           mobileWebUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
           webUrl: `https://gallae-fe.vercel.app/posts/${postId}`,
         },
       },
       social: {},
-      buttons: [], // 빈 배열로 설정하여 버튼을 숨깁니다.
+      buttons: [
+        {
+          title: "",
+          link: {
+            mobileWebUrl: "",
+            webUrl: "",
+          },
+        },
+        {
+          title: "",
+          link: {
+            mobileWebUrl: "",
+            webUrl: "",
+          },
+        },
+      ],
     });
   }
 };
