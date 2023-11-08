0cae029c37cb965de1c85ccac47f03675f438205
Won <hwon466@gmail.com>
2023-11-08 21:36:59 +0900

    Feat: 대댓글 수정 삭제 기능 구현

--- src/components/postDetailsPage/EditDeleteModal.jsx
+++ src/components/postDetailsPage/EditDeleteModal.jsx
@@ -1,4 +1,6 @@
 export default function EditDeleteModal({
+  isComment = false,
+  isReplyComment = false,
   commentId,
   contents,
   handleDelete,
@@ -11,16 +13,24 @@ export default function EditDeleteModal({
       <div
         className="pl-3 py-2 border-b-[1px] border-[#D9D9D9]"
         onClick={() => {
-          setCommentType("edit");
-          setNewComment({ contents });
+          if (isComment) {
+            setCommentType("edit");
+            setNewComment({ contents });
+          }
+
+          if (isReplyComment) {
+            setCommentType("replyEdit");
+            setNewComment({ contents });
+          }
+          // 모달 내의 입력 창에 포커스
           if (editedContentRef && editedContentRef.current) {
-            editedContentRef.current.focus(); // 모달 내의 입력 창에 포커스를 줍니다.
+            editedContentRef.current.focus();
           }
         }}
       >
         댓글 수정
       </div>
-      <div className="pl-3 py-2" onClick={() => handleDelete(commentId)}>
+      <div className="pl-3 py-2" onClick={handleDelete}>
         댓글 삭제
       </div>
     </div>
