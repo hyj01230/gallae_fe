4bfae66c960b4b3c3846dc7a4e07bded42590809
Won <hwon466@gmail.com>
2023-11-11 04:45:53 +0900

    Feat: 답글 수정시 포커스 가게 하기

--- src/components/postDetailsPage/Comments.jsx
+++ src/components/postDetailsPage/Comments.jsx
@@ -339,6 +339,7 @@ export default function Comments({
                                 setCommentType={setCommentType}
                                 setNewComment={setNewComment}
                                 handleDelete={handleDeleteReply}
+                                editedContentRef={editedContentRef}
                               />
                             ) : (
                               <></>
