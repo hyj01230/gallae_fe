967f7922955defecfdb5a88d54fca3cd04815f16
hongmikyeong <mikyeong.dev@gmail.com>
2023-11-10 13:25:27 +0900

    코드 복구

--- src/store/atom.js
+++ src/store/atom.js
@@ -4,3 +4,9 @@ export const scheduleState = atom({
   key: "scheduleState",
   default: {},
 });
+
+// 전역으로 사용할 state를 선언
+export const nickNameState = atom({
+  key: "nickNameState",
+  default: "",
+});
